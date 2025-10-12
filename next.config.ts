5d64d91453c0587a26ad5e89e08ae32667b1be11
MarcSmg <dossagaby@gmail.com>
2025-10-12 22:31:58 +0100

    modified webpack in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.externals.push('prisma');
-        }
+  webpack: (config, { isServer }) => {
+    // Only apply this fix on the server-side compilation
+    if (isServer) {
+      // Treat the Prisma client library as external so Webpack doesn't try to bundle it
+      // This is crucial for fixing the "require() style import is forbidden" error
+      config.externals.push('@prisma/client', '.prisma/client');
+    }
         return config;
     },
 };
